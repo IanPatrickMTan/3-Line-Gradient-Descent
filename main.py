18a015b54d1d2e009391ba3457e797efb362a525
Ian Patrick M. Tan <ianpatricktan@gmail.com>
2021-07-25 04:56:04 -0400

    yeah fuck you Koral
    GET FUCKED

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-def f(x):global y;return eval(y)
-x=float(input('x = '));y=input('y = ')
-for iter in range(100):x-=(f(x+0.001)-f(x))/0.001*0.1;print('\nx =',x,'\ny =',f(x))
+x,y,t=float(input('x = ')),input('y = '),int(input('Iterations: '));exec("for iter in range(t):x-=(eval(y.replace('x', '(x + 0.001)'))-eval(y))/0.001*0.1;print('\\nx =',x,'\\ny =',eval(y))")
